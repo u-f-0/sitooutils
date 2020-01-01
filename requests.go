1a56ac1c8ad12c86ba9d2a5a42c4ad8036e5c21c
ufo666 <carl@u-f-o.se>
2020-01-01 13:13:19 +0100

    Addition of node function and logrus logging

--- requests.go
+++ requests.go
@@ -4,12 +4,12 @@ package sitooutils
 import (
 	"bytes"
 	"encoding/base64"
-	"fmt"
-	log "github.com/sirupsen/logrus"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
+
+	log "github.com/sirupsen/logrus"
 )
 
 //BasicAuth - Functon to convert string base64
@@ -18,24 +18,28 @@ func BasicAuth(user, pass string) string {
 	return base64.StdEncoding.EncodeToString([]byte(auth))
 }
 
-//GetSitoo - Function to GET data from Sitoo
-func GetSitoo(endpoint string, account string, password string) []byte {
-	accountNo := strings.Split(account, "-")
-
+func Node(accountNo string) string {
 	var node string
-	if string(accountNo[0][0]) == "9" {
+	if string(accountNo[0]) == "9" {
 		node = "-sandbox"
-	} else if string(accountNo[0][0:3]) == "130" {
+	} else if string(accountNo[0:3]) == "130" {
 		node = "130"
-	} else if string(accountNo[0][0:3]) == "100" {
+	} else if string(accountNo[0:3]) == "100" {
 		node = ""
-	} else if string(accountNo[0][0:3]) == "201" {
+	} else if string(accountNo[0:3]) == "201" {
 		node = "201"
-	} else if string(accountNo[0][0:3]) == "202" {
+	} else if string(accountNo[0:3]) == "202" {
 		node = "202"
 	}
+	return node
+}
 
-	req, err := http.NewRequest("GET", "https://api"+node+".mysitoo.com/v2/accounts/"+accountNo[0]+endpoint, nil)
+//GetSitoo - Function to GET data from Sitoo
+func GetSitoo(endpoint string, account string, password string) []byte {
+	accountSplit := strings.Split(account, "-")
+	accountNo := accountSplit[0]
+
+	req, err := http.NewRequest("GET", "https://api"+Node(accountNo)+".mysitoo.com/v2/accounts/"+accountNo+endpoint, nil)
 	req.Header.Add("content-type", "application/json")
 	req.Header.Add("Authorization", "Basic "+BasicAuth(account, password))
 	resp, err := http.DefaultClient.Do(req)
@@ -84,36 +88,51 @@ func GetSitoo(endpoint string, account string, password string) []byte {
 
 //PostSitoo - Function to POST data to Sitoo
 func PostSitoo(endpoint string, account string, password string, json []byte) []byte {
-	accountNo := strings.Split(account, "-")
+	accountSplit := strings.Split(account, "-")
+	accountNo := accountSplit[0]
 
-	var node string
-	if string(accountNo[0][0]) == "9" {
-		node = "-sandbox"
-	} else if string(accountNo[0][0:3]) == "130" {
-		node = "130"
-	} else if string(accountNo[0][0:3]) == "100" {
-		node = ""
-	} else if string(accountNo[0][0:3]) == "201" {
-		node = "201"
-	} else if string(accountNo[0][0:3]) == "202" {
-		node = "202"
-	}
-	req, err := http.NewRequest("POST", "https://api"+node+".mysitoo.com/v2/accounts/"+accountNo[0]+endpoint, bytes.NewBuffer(json))
+	req, err := http.NewRequest("POST", "https://api"+Node(accountNo)+".mysitoo.com/v2/accounts/"+accountNo+endpoint, bytes.NewBuffer(json))
 	req.Header.Add("content-type", "application/json")
 	req.Header.Add("Authorization", "Basic "+BasicAuth(account, password))
 	resp, err := http.DefaultClient.Do(req)
 
+	log.WithFields(log.Fields{
+		"requesttype": "POST",
+		"account":     account,
+		"endpoint":    endpoint,
+		"body":        json,
+	}).Info("Request sent")
+
 	if err != nil {
-		panic(err)
+		log.WithFields(log.Fields{
+			"requesttype": "POST/Error",
+			"account":     account,
+			"endpoint":    endpoint,
+			"body":        json,
+			"response":    err,
+		}).Fatal("ERROR")
 	}
 	defer resp.Body.Close()
 
 	response, _ := ioutil.ReadAll(resp.Body)
 	if resp.StatusCode != 200 {
-		fmt.Println("StatusCode:", resp.StatusCode)
-		log.Fatal(string(response))
+		log.WithFields(log.Fields{
+			"requesttype": "POST/Response",
+			"account":     account,
+			"endpoint":    endpoint,
+			"statuscode":  resp.StatusCode,
+			"response":    string(response),
+		}).Fatal("ERROR")
+
 		os.Exit(1)
 	} else {
+		log.WithFields(log.Fields{
+			"requesttype": "POST/Response",
+			"account":     account,
+			"endpoint":    endpoint,
+			"statuscode":  resp.StatusCode,
+			"response":    string(response),
+		}).Info("OK")
 		return response
 	}
 	return response
@@ -121,36 +140,51 @@ func PostSitoo(endpoint string, account string, password string, json []byte) []
 
 //PutSitoo - Function to PUT data to Sitoo
 func PutSitoo(endpoint string, account string, password string, json []byte) []byte {
-	accountNo := strings.Split(account, "-")
+	accountSplit := strings.Split(account, "-")
+	accountNo := accountSplit[0]
 
-	var node string
-	if string(accountNo[0][0]) == "9" {
-		node = "-sandbox"
-	} else if string(accountNo[0][0:3]) == "130" {
-		node = "130"
-	} else if string(accountNo[0][0:3]) == "100" {
-		node = ""
-	} else if string(accountNo[0][0:3]) == "201" {
-		node = "201"
-	} else if string(accountNo[0][0:3]) == "202" {
-		node = "202"
-	}
-	req, err := http.NewRequest("PUT", "https://api"+node+".mysitoo.com/v2/accounts/"+accountNo[0]+endpoint, bytes.NewBuffer(json))
+	req, err := http.NewRequest("PUT", "https://api"+Node(accountNo)+".mysitoo.com/v2/accounts/"+accountNo+endpoint, bytes.NewBuffer(json))
 	req.Header.Add("content-type", "application/json")
 	req.Header.Add("Authorization", "Basic "+BasicAuth(account, password))
 	resp, err := http.DefaultClient.Do(req)
 
+	log.WithFields(log.Fields{
+		"requesttype": "PUT",
+		"account":     account,
+		"endpoint":    endpoint,
+		"body":        json,
+	}).Info("Request sent")
+
 	if err != nil {
-		panic(err)
+		log.WithFields(log.Fields{
+			"requesttype": "PUT/Error",
+			"account":     account,
+			"endpoint":    endpoint,
+			"body":        json,
+			"response":    err,
+		}).Fatal("ERROR")
 	}
 	defer resp.Body.Close()
 
 	response, _ := ioutil.ReadAll(resp.Body)
 	if resp.StatusCode != 200 {
-		fmt.Println("StatusCode:", resp.StatusCode)
-		log.Fatal(string(response))
+		log.WithFields(log.Fields{
+			"requesttype": "PUT/Response",
+			"account":     account,
+			"endpoint":    endpoint,
+			"statuscode":  resp.StatusCode,
+			"response":    string(response),
+		}).Fatal("ERROR")
+
 		os.Exit(1)
 	} else {
+		log.WithFields(log.Fields{
+			"requesttype": "PUT/Response",
+			"account":     account,
+			"endpoint":    endpoint,
+			"statuscode":  resp.StatusCode,
+			"response":    string(response),
+		}).Info("OK")
 		return response
 	}
 	return response
